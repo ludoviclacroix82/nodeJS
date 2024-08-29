37c3cfdaa803285f6f639ea7715473b499e1b6a1
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-08-29 12:10:28 +0200

    started

---
+++ 1.Started/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end('<h1>Hello World</h1>');
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
